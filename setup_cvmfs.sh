7e9040c8e8916ea9085b7ea786d4979513d4c6af
Gabriel Pinheiro Matos <gpinheir@lxplus910.cern.ch>
2024-05-25 19:09:56 +0200

    Working software releases for compiling COCOA on EL9

--- setup_cvmfs.sh
+++ setup_cvmfs.sh
@@ -1,13 +1,16 @@
+# Software releases for COCOA to work on EL9
+# NOTE: this requires Geant4 version >11, some header files in the original code had to be shuffled
 export CURRENTDIR=$(pwd)
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/Geant4-env.sh
-cd /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/share/Geant4-10.5.1/geant4make/
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/Geant4/10.05.p01.1/x86_64-centos7-gcc8-opt/share/Geant4-10.5.1/geant4make/geant4make.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/ROOT/6.18.04/x86_64-centos7-gcc8-opt/ROOT-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/HepMC/2.06.10/x86_64-centos7-gcc8-opt/HepMC-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/jsoncpp/1.8.4/x86_64-centos7-gcc8-opt/jsoncpp-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/CMake/3.14.3/x86_64-centos7-gcc8-opt/CMake-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/cmaketools/1.8/x86_64-centos7-gcc8-opt/cmaketools-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/301/x86_64-centos7-gcc8-opt/pythia8env-genser.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/fastjet/3.3.2/x86_64-centos7-gcc8-opt/fastjet-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/Geant4/11.2.0/x86_64-el9-gcc13-opt/Geant4-env.sh
+cd /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/Geant4/11.2.0/x86_64-el9-gcc13-opt/share/Geant4/geant4make
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/Geant4/11.2.0/x86_64-el9-gcc13-opt/share/Geant4/geant4make/geant4make.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/ROOT/6.30.06/x86_64-el9-gcc13-opt/ROOT-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/HepMC/2.06.11/x86_64-el9-gcc13-opt/HepMC-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/jsoncpp/1.9.3/x86_64-el9-gcc13-opt/jsoncpp-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/CMake/3.26.2/x86_64-el9-gcc13-opt/CMake-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/cmaketools/1.9/x86_64-el9-gcc13-opt/cmaketools-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/MCGenerators/pythia8/310/x86_64-el9-gcc13-opt/pythia8env-genser.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_105b/fastjet/3.4.1/x86_64-el9-gcc13-opt/fastjet-env.sh
+export PYTHIA8_HOME=/cvmfs/sft.cern.ch/lcg/releases/LCG_105b/MCGenerators/pythia8/310/x86_64-el9-gcc13-opt/
 export PYTHIA8DATA=$PYTHIA8_HOME/share/Pythia8/xmldoc   
 cd $CURRENTDIR
